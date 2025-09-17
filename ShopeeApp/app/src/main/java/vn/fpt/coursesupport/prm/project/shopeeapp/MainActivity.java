84e4f3b64d9530f177fe8ffc94338b39c78eb45c
zondat <datnguyen101191@gmail.com>
2025-09-17 13:38:22 +0700

    Load drawable from fragment

--- ShopeeApp/app/src/main/java/vn/fpt/coursesupport/prm/project/shopeeapp/MainActivity.java
+++ ShopeeApp/app/src/main/java/vn/fpt/coursesupport/prm/project/shopeeapp/MainActivity.java
@@ -1,6 +1,7 @@
 package vn.fpt.coursesupport.prm.project.shopeeapp;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
@@ -9,6 +10,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentTransaction;
+
+import vn.fpt.coursesupport.prm.project.shopeeapp.view.ViewProductImage;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +25,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         viewHome = findViewById(R.id.viewHome);
-        LinearLayout pnHomeProductLeft = viewHome.findViewById(R.id.panelProductLeft);
-        
+        LinearLayout panelProductLeft = viewHome.findViewById(R.id.panelProductLeft);
+
+        ViewProductImage productChocopie = new ViewProductImage();
+        Bundle args = new Bundle();
+        args.putString("productName", "Chocopie");
+        args.putString("drawableId", "product_chocopie");
+        productChocopie.setArguments(args);
+
+        // Begin fragment transaction
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(panelProductLeft.getId(), productChocopie);
+        transaction.addToBackStack("fragmentProductChocopie");
+        transaction.commit();
+
+        ViewProductImage productChipChip = new ViewProductImage();
+        args = new Bundle();
+        args.putString("productName", "Keo Chip Chip");
+        args.putString("drawableId", "product_chip_chip");
+        productChipChip.setArguments(args);
+
+        // Begin fragment transaction
+        transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(panelProductLeft.getId(), productChipChip);
+        transaction.addToBackStack("fragmentProductChipChip");
+        transaction.commit();
+
     }
 }
--- ShopeeApp/app/src/main/java/vn/fpt/coursesupport/prm/project/shopeeapp/view/ViewProductImage.java
+++ ShopeeApp/app/src/main/java/vn/fpt/coursesupport/prm/project/shopeeapp/view/ViewProductImage.java
@@ -1,11 +1,14 @@
 package vn.fpt.coursesupport.prm.project.shopeeapp.view;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -25,9 +28,8 @@ import vn.fpt.coursesupport.prm.project.shopeeapp.R;
 public class ViewProductImage extends Fragment {
 
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PRODUCT_ID = "Id";
     private static final String ARG_PRODUCT_NAME = "productName";
-    private static final String ARG_DRAWABLE_PATH = "drawableId";
+    private static final String ARG_DRAWABLE_ID = "drawableId";
 
     // TODO: Rename and change types of parameters
     private TextView txtViewProduct;
@@ -37,18 +39,11 @@ public class ViewProductImage extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment ViewProductImage.
-     */
-    public static ViewProductImage newInstance(String id, String name, String drawablePath) {
+    public static ViewProductImage newInstance(String id, String name, String drawableId) {
         ViewProductImage fragment = new ViewProductImage();
         Bundle args = new Bundle();
-        args.putString(ARG_PRODUCT_ID, id);
         args.putString(ARG_PRODUCT_NAME, name);
-        args.putString(ARG_DRAWABLE_PATH, drawablePath);
+        args.putString(ARG_DRAWABLE_ID, drawableId);
         fragment.setArguments(args);
         return fragment;
     }
@@ -68,30 +63,23 @@ public class ViewProductImage extends Fragment {
 
         if (getArguments() != null) {
             String productName = getArguments().getString(ARG_PRODUCT_NAME);
-            String imagePath = getArguments().getString(ARG_DRAWABLE_PATH);
+            String drawableId = getArguments().getString(ARG_DRAWABLE_ID);
 
             txtViewProduct.setText(productName);
-            imgViewProduct.setImageDrawable(getDrawableFromFilePath(imagePath));
+            imgViewProduct.setImageDrawable(loadDrawableByName(getContext(), drawableId));
         }
 
         return view;
     }
 
-    public Drawable getDrawableFromFilePath(String filePath) {
-        try {
-            // Create a Bitmap from the file path
-            Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+    public static Drawable loadDrawableByName(Context context, String name) {
+        Resources resources = context.getResources();
+        int drawableId = resources.getIdentifier(name, "drawable", context.getPackageName());
 
-            if (bitmap != null) {
-                // Convert Bitmap to Drawable
-                return new BitmapDrawable(getResources(), bitmap);
-            } else {
-                // Handle the case where the bitmap couldn't be loaded
-                Log.e("DrawableUtils", "Failed to load bitmap from path: " + filePath);
-                return null;
-            }
-        } catch (Exception e) {
-            Log.e("DrawableUtils", "Error creating drawable from file path: " + e.getMessage());
+        if (drawableId != 0) {
+            return ResourcesCompat.getDrawable(resources, drawableId, null);
+        } else {
+            // Handle case where drawable is not found
             return null;
         }
     }
